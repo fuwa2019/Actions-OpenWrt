9438515068b21dd6f3e0998862ac7257a7e18b87
fuwa2019 <56064365+fuwa2019@users.noreply.github.com>
2023-01-19 17:26:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,9 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome-rufengsuixing
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
