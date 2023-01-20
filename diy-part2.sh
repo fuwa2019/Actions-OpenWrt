6edf9fa275865217a73b629afecc8d5fcde722f9
fuwa2019 <56064365+fuwa2019@users.noreply.github.com>
2023-01-20 14:58:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
@@ -39,7 +28,6 @@ cat << EOF > package/base-files/files/etc/rc.local
 # the system init finished. By default this file does nothing.
 chmod 0775 /etc/init.d/lucky 
 chmod 0775 /usr/bin/AdGuardHome/AdGuardHome
+rm -f /etc/init.d/adguardhome
 exit 0
 EOF
-#7. Add packages
-git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
